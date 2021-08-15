224977997a337101366b5617660be144f4bb29a4
pome-ta <pometarou93@gmail.com>
2021-08-15 22:25:44 +0900

    update

--- 04/04-2_合計値-084.py
+++ 04/04-2_合計値-084.py
@@ -1,14 +1,12 @@
 # 合計値を求める
 
-
 def list_sum(array: list) -> int:
-  sum_value: int = 0
-  for num in array:
-    sum_value += num
-  return sum_value
+    sum_value: int = 0
+    for num in array:
+        sum_value += num
+    return sum_value
 
 
 if __name__ == '__main__':
-  a: list = [1, 3, 10, 2, 8]
-  print(list_sum(a))
-
+    a: list = [1, 3, 10, 2, 8]
+    print(list_sum(a))
