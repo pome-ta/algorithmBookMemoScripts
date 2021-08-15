824a552b9923167dfcd9738870d5952885d912bd
pome-ta <pome-ta@users.noreply.github.com>
2021-08-15 19:38:47 +0900

    04-2_合計値-084.py

---
+++ 04/04-2_合計値-084.py
@@ -0,0 +1,14 @@
+# 合計値を求める
+
+
+def list_sum(array: list) -> int:
+  sum_value: int = 0
+  for num in array:
+    sum_value += num
+  return sum_value
+
+
+if __name__ == '__main__':
+  a: list = [1, 3, 10, 2, 8]
+  print(list_sum(a))
+
--- 04/04-2_合計値-84.py
+++
@@ -1,5 +0,0 @@
-a = [1, 3, 10, 2, 8]
-sum_value = 0
-for i in a:
-  sum_value += i
-print(sum_value)
