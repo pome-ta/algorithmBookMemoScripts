27148d177fccac257270ef62598bcc359516fa73
pome-ta <pometarou93@gmail.com>
2021-08-15 22:33:01 +0900

    update

--- 04/04-3_平均値-095.py
+++ 04/04-3_平均値-095.py
@@ -1,16 +1,13 @@
 # 平均値を求める
 
-
 def list_average(array: list) -> float:
-  len_array: int = len(array)
-  sum_value: int = 0
-  for num in array:
-    sum_value += num
-
-  return sum_value / len_array
+    len_array: int = len(array)
+    sum_value: int = 0
+    for num in array:
+        sum_value += num
+    return sum_value / len_array
 
 
 if __name__ == '__main__':
-  a: list = [1, 3, 10, 2, 8]
-  print(list_average(a))
-
+    a: list = [1, 3, 10, 2, 8]
+    print(list_average(a))
